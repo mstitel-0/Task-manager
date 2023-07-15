2c31a64b79a213578641802299bcfc5341b4a62f
Yaroslav <jarik012005@gmail.com>
2023-07-15 23:44:55 +0200

    jwt token updated
    Handled different exceptions during Jwt token manipulations, fixed logger code

--- src/main/java/com/mstitel/timemanager/JwtUtils.java
+++ src/main/java/com/mstitel/timemanager/JwtUtils.java
@@ -1,22 +1,21 @@
 package com.mstitel.timemanager;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 
 import java.util.Date;
-import java.util.logging.Logger;
 import java.security.Key;
 
 @Component
 public class JwtUtils {
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(JwtUtils.class);
+    private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
     @Value("${jwtSecret}")
     private String jwtPass ;
@@ -38,6 +37,31 @@ public class JwtUtils {
 
     private Key key(){
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtPass));
+    }
+
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parserBuilder().setSigningKey(key())
+                .build().parseClaimsJwt(token)
+                .getBody().getSubject();
+    }
+    public boolean validateJwtToken(String authToken){
+        try{
+            Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJwt(authToken);
+            return true;
+        }
+        catch (MalformedJwtException e){
+            logger.error("Invalid token: {}" , e.getMessage());
+        }
+        catch (ExpiredJwtException e){
+            logger.error("Token is expired: {}" , e.getMessage());
+        }
+        catch (UnsupportedJwtException e){
+            logger.error("Unsupported token: {}" , e.getMessage());
+        }
+        catch (IllegalArgumentException e){
+            logger.error("Token is empty: {} ", e.getMessage());
+        }
+        return false;
     }
 
 
